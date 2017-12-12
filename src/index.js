1038fea3505fd603d43faf5a8ac3abc697fce6e1
Thomas Bianchini <thomas.bianchini@stormshield.eu>
2017-12-12 16:43:49 +0100

    [REACT] delete classmate

--- src/index.js
+++ src/index.js
@@ -22,38 +22,53 @@ class StudentCard extends React.Component {
           <h5 className="student-birthdate">{this.props.birthdate}</h5>
         </div>
         <div className="sc-footer">
-          <button className="btn edit-student-btn"><img src={edit} alt=""/></button>
+          <button className="btn edit-student-btn"><img src={edit} alt=""/></button>
           <div className="empty-space"></div>
-          <button className="btn delete-student-btn"><img src={deletePicture} alt=""/></button>
+          <button className="btn delete-student-btn"><img onClick={() => this.props.onDeleteClick()} src={deletePicture} alt=""/></button>
         </div>
       </div>
     );
   }
 }
 
-function Classroom (props) {
-  const studentsGiven = props.students;
-  const students = studentsGiven.map(student => 
-    <StudentCard key={student.id}
-                  sex={student.sex} 
-                  average={student.average} 
-                  lastname={student.lastname} 
-                  firstname={student.firstname} 
-                  birthdate={student.birthdate}
-    />
-  );
-  return (
-    <div className="classroom">
-      <div className="cl-header">
-        <h1>My Classroom</h1>
+class Classroom extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      students: props.students
+    }
+  }
+
+  createStudentCards(){
+    return this.state.students.map(student => 
+      <StudentCard key={student.id}
+                    sex={student.sex} 
+                    average={student.average} 
+                    lastname={student.lastname} 
+                    firstname={student.firstname}
+                    onDeleteClick={() => this.handleDeleteClick(student.id)}
+      /> );
+  };
+
+  handleDeleteClick(id) {
+     this.setState({students: this.state.students.slice().filter(student => student.id !== id)});
+  }
+
+  render() {
+    return (
+      <div className="classroom">
+        <div className="cl-header">
+          <h1>My Classroom</h1>
+        </div>
+        <div className="students">
+          {this.createStudentCards()}
+        </div>
+        <div className="footer">
+        </div>
       </div>
-      <div className="students">
-        {students}
-      </div>
-      <div className="footer">
-      </div>
-    </div>
-  );
+    );
+  } 
 }
 
 
@@ -63,36 +78,31 @@ function Classroom (props) {
     sex:'boy',
     average:12,
     lastname:'Bianchini',
-    firstname:'Thomas',
-    birthdate:'01/09/1993'
-  },{
-    id:0,
-    sex:'girl',
-    average: 16,
-    lastname:'Roques',
-    firstname:'Chloé',
-    birthdate:'15/02/1996'
+    firstname:'Thomas'
   },{
     id:1,
     sex:'girl',
-    average: 18,
-    lastname:'Zgueg',
-    firstname:'Malika',
-    birthdate:'08/05/1995'
+    average: 16,
+    lastname:'Roques',
+    firstname:'Chloé'
   },{
     id:2,
     sex:'girl',
-    average: 8,
-    lastname:'Alice',
-    firstname:'Coraya',
-    birthdate:'10/12/1993'
+    average: 18,
+    lastname:'Zgueg',
+    firstname:'Malika'
   },{
     id:3,
+    sex:'girl',
+    average: 8,
+    lastname:'Alice',
+    firstname:'Coraya'
+  },{
+    id:4,
     sex:'boy',
     average: 14,
     lastname:'Francois',
-    firstname:'Timoté',
-    birthdate:'22/06/1993'
+    firstname:'Timoté'
   },];
   ReactDOM.render(
     <Classroom students={studentsMock}/>,
